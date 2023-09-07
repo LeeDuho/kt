681769d8013330e6b74e956af86a58463c02d90b
duho <dl6874@gmail.com>
2023-09-07 10:16:47 +0900

    init

---
+++ src/main/java/com/restful/webservices/restfulapiservices/RestfulApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.restful.webservices.restfulapiservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restful/webservices/restfulapiservices/RestfulApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.webservices.restfulapiservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
